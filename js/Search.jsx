4a8f3045b216d87367f5fc987c9f92ffc60cc2a7
Christiaan Quyn <christianq010@gmail.com>
2018-08-03 19:17:44 +0530

    go upto key props

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+// components
+import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
 const Search = () => (
   <div className="search">
-    {/* Testing response below */}
-    {/* <pre><code> {JSON.stringify(preload, null, 4)} </code></pre> */}
-    {preload.shows.map(show => <h3>{show.title}</h3>)}
+    {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
   </div>
 );
 
