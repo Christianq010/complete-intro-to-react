4d13476e001099755704cb8cbcc8c4e1779f390b
Christiaan Quyn <christianq010@gmail.com>
2018-08-14 14:12:12 +0530

    Fix: display card details route

--- js/App.jsx
+++ js/App.jsx
@@ -24,7 +24,7 @@ const App = () => (
           path="/details/:id"
           component={(props: { match: Match }) => {
             const selectedShow = preload.shows.find(
-              (show: Show) => props.params.id === show.imdbID
+              show => props.match.params.id === show.imdbID
             );
             return <Details show={selectedShow} {...props} />;
           }}
